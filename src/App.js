5831f303d395a72a1b19bb5c9605b898a7873bb8
602822 <89077126+602822@users.noreply.github.com>
2024-02-15 19:45:45 +0100

    added weatherWidget

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Stack } from "@mui/material";
 import Description from "./components/Description";
 import ImageComponent from "./components/ImageComponent";
 import ToDoList from "./components/ToDoList";
-
+import DailyQuote from "./components/DailyQuote";
+import WeatherWidget from "./components/WeatherWidget";
 export default function App() {
   return (
     <Container maxWidth="lg">
@@ -13,7 +14,9 @@ export default function App() {
       <Stack direction="column" spacing={2} mt={2}>
         <ToDoList></ToDoList>
         <Description />
+        <DailyQuote></DailyQuote>
         <ImageComponent />
+        <WeatherWidget></WeatherWidget>
       </Stack>
     </Container>
   );
--- src/components/WeatherWidget/helpers.js
+++ src/components/WeatherWidget/helpers.js
@@ -14,5 +14,8 @@ export const fetchNowcast = async ({ lat, lon }) => {
 };
 
 export const locations = {
-  Bergen: {},
+  Bergen: {
+    lat: 60,
+    lon: 5,
+  },
 };
